303fa7ee52571c65d67cde604bcff0f1f4d0e898
SamusVD <samueldanso@outlook.fr>
2020-03-02 14:47:13 +0100

    CheckPassword added

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -17,14 +17,46 @@ export class SignInComponent implements OnInit {
 
   createUser(){
     console.log("createUser CALLED");
-    let username = (<HTMLInputElement>document.getElementById("username")).value;
-    let email = (<HTMLInputElement>document.getElementById("email")).value;
     let password = (<HTMLInputElement>document.getElementById("password")).value;
-    let displayname = ""
-    let isAdmin = false
-    let user= new User(username,displayname,password,email,isAdmin);
-    this.userService.createUser(user)
-    console.log(user);
+    let result = this.checkPassword(password);
+    console.log(result);
+    if(result===passwordEnum.ok){
+      let username = (<HTMLInputElement>document.getElementById("username")).value;
+      let email = (<HTMLInputElement>document.getElementById("email")).value;
+      let displayname = "";
+      let isAdmin = false;
+      let user= new User(username,displayname,password,email,isAdmin);
+      this.userService.createUser(user);
+    }else if (result===passwordEnum.notMatching){
+      console.log("Password not matching");
+    }else if (result===passwordEnum.badPassword){
+      console.log("Bad password");
+    }
+    
   }
 
+  private checkPassword(password: string): passwordEnum{
+    let passwordConfirm = (<HTMLInputElement>document.getElementById("passwordConfirm")).value;
+    console.log(password ,"+", passwordConfirm)
+    if (password===passwordConfirm){
+      let regex= new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
+      let bool = regex.test(password);
+      console.log(bool)
+      if(bool===true){
+      return passwordEnum.ok;
+      }else{
+        return passwordEnum.badPassword;
+      }
+    } else{
+      return passwordEnum.notMatching;
+    }
+    
+  }
+
+}
+
+enum passwordEnum{
+  ok = "OK",
+  notMatching= "NOT_MATCHING",
+  badPassword= "BAD_PASSWORD"
 }
